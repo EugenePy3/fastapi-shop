7876cc4f68f1bd7b039b3fdb38f7c5bc405c9cd2
Eugene <ruskeep@bk.ru>
2025-12-08 16:44:26 +0700

    add schemas/repositories/services

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -4,20 +4,23 @@ from datetime import datetime
 
 from ..database import Base
 
+
 class Product(Base):
     __tablename__ = 'products'
+
 
 id = Column(Integer, primary_key=True, index=True)
 name = Column(String, nullable=False, index=True)
 description = Column(Text)
 price = Column(Float, nulltable=False)
 
-category_id = Column(Integer, ForeignKey('Categories.id'), nullable=False)
+category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
 
 image_url = Column(String)
 created_at = Column(DateTime, default=datetime.utcnow)
 
-category = relationship('Category', back_populates='products')
+category = relationship('Category', back_populates='product')
+
 
 def __repr__(self):
     return f'<Product(id={self.id}, name={self.name}), price={self.price}>'
